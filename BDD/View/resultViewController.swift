6727f1b2a0971111b7767868ea3fc270a6df26e2
Fabricio Pujol <Fabricio_2112@hotmail.com>
2019-09-05 17:09:48 -0300

    create layout result screen

---
+++ BDD/View/resultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  resultViewController.swift
+//  BDD
+//
+//  Created by Fabricio Pujol on 05/09/19.
+//  Copyright © 2019 LeonardoBSR. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
